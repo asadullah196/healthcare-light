32205239c077de9753e646559a52fc9b08352f4c
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-11 22:34:04 +0600

    First hello world type program done!

--- includes/Admin/menu.php
+++ includes/Admin/menu.php
@@ -1,3 +1,20 @@
 <?php
 
 namespace Galib1996\HealthcareLite\Admin;
+
+/**
+ * The menu handler class
+ */
+class Menu {
+    function __construct(){
+        add_action( 'admin_menu', [$this,'admin_menu'] );
+    }
+
+    public function admin_menu(){
+        add_menu_page( __('HealthCare Lite','healthcare-lite'), __('HealthCare','healthcare-lite'), 'manage_options', 'healthcare-lite', [$this, 'plugin_page'], 'dashicons-image-filter');
+    }
+
+    public function plugin_page(){
+        echo "Hello Doctors!";
+    }
+}

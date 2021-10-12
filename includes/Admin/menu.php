bab5b333f87f3c3fcc494d7fe59a51773a41e3eb
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-12 20:37:50 +0600

    Code optimize

--- includes/Admin/menu.php
+++ includes/Admin/menu.php
@@ -21,16 +21,16 @@ class Menu
         $parent_slug = 'healthcare-lite';
         $capability = 'manage_options';
 
-        add_menu_page(__('Healthcare Lite', 'healthcare-lite'), __('Healthcare Lite', 'healthcare-lite'), $capability, $parent_slug, [$this, 'healthcare_dashboard_doc'], 'dashicons-image-filter', 20);
+        add_menu_page(__('Healthcare Lite', 'healthcare-lite'), __('Healthcare Lite', 'healthcare-lite'), $capability, $parent_slug, [$this, 'healthcare_dashboard'], 'dashicons-image-filter', 20);
 
-        add_submenu_page($parent_slug, __('Dashboard', 'healthcare-lite'), __('Dashboard', 'healthcare-lite'), $capability, $parent_slug, [$this, 'healthcare_dashboard_doc']);
+        add_submenu_page($parent_slug, __('Dashboard', 'healthcare-lite'), __('Dashboard', 'healthcare-lite'), $capability, $parent_slug, [$this, 'healthcare_dashboard']);
         add_submenu_page($parent_slug, __('Add Doctor', 'healthcare-lite'), __('Add Doctor', 'healthcare-lite'), $capability, 'healthcare-lite-add-doc', [$this, 'add_doctor']);
     }
 
     /**
      * healthcare_dashboard_doc call back define
      */
-    public function healthcare_dashboard_doc()
+    public function healthcare_dashboard()
     {
         $doctors = new Dashboard();
         $doctors->admin_dashboard();

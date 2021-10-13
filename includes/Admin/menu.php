3f3981e013981d91417820e57f31bad25104fa0e
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-13 23:29:57 +0600

    Re-structure Admin Pannel!

--- includes/Admin/menu.php
+++ includes/Admin/menu.php
@@ -22,19 +22,27 @@ class Menu
         $capabilityAdmin = 'manage_options';
         $capabilityEditor = 'editor';
 
-        add_menu_page(__('Healthcare Lite', 'healthcare-lite'), __('Healthcare Lite', 'healthcare-lite'), $capabilityAdmin, $parent_slug, [$this, 'healthcare_dashboard'], 'dashicons-image-filter', 20);
+        add_menu_page(__('Healthcare Lite', 'healthcare-lite'), __('Healthcare Lite', 'healthcare-lite'), $capabilityAdmin, $parent_slug, [$this, 'healthcare_admin_dashboard'], 'dashicons-image-filter', 20);
 
-        add_submenu_page($parent_slug, __('Dashboard', 'healthcare-lite'), __('Dashboard', 'healthcare-lite'), $capabilityAdmin, $parent_slug, [$this, 'healthcare_dashboard']);
+        add_submenu_page($parent_slug, __('Dashboard', 'healthcare-lite'), __('Dashboard', 'healthcare-lite-dashboard'), $capabilityAdmin, $parent_slug, [$this, 'healthcare_admin_dashboard']);
 
-        add_submenu_page($parent_slug, __('Add Doctor', 'healthcare-lite'), __('Add Doctor', 'healthcare-lite'), $capabilityAdmin, 'healthcare-lite-add-doc', [$this, 'add_doctor']);
+        add_submenu_page($parent_slug, __('Doctor', 'healthcare-lite'), __('Doctor', 'healthcare-lite'), $capabilityAdmin, 'healthcare-lite-doctor', [$this, 'healthcare_admin_doctor']);
 
-        add_submenu_page($parent_slug, __('Add Patient', 'healthcare-lite'), __('Add Patient', 'healthcare-lite'), $capabilityEditor, 'healthcare-lite-add-patient', [$this, 'add_patient']);
+        add_submenu_page($parent_slug, __('Patient', 'healthcare-lite'), __('Patient', 'healthcare-lite'), $capabilityAdmin, 'healthcare-lite-patient', [$this, 'healthcare_admin_patient']);
+
+        add_submenu_page($parent_slug, __('Technologist', 'healthcare-lite'), __('Technologist', 'healthcare-lite'), $capabilityAdmin, 'healthcare-lite-technologist', [$this, 'healthcare_admin_technologist']);
+
+        add_submenu_page($parent_slug, __('Appointment', 'healthcare-lite'), __('Appointment', 'healthcare-lite'), $capabilityAdmin, 'healthcare-lite-appointment', [$this, 'healthcare_admin_appintment']);
+
+        add_submenu_page($parent_slug, __('Profile', 'healthcare-lite'), __('Profile', 'healthcare-lite'), $capabilityAdmin, 'healthcare-lite-profile', [$this, 'healthcare_admin_profile']);
+
+        
     }
 
     /**
      * healthcare_dashboard_doc call back define
      */
-    public function healthcare_dashboard()
+    public function healthcare_admin_dashboard()
     {
         $doctors = new Dashboard();
         $doctors->admin_dashboard();

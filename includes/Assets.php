193fff96092cfa221e02e96f664b7421c304231c
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-17 00:24:13 +0600

    CSS file added and lead on fron end successfully!

--- includes/Assets.php
+++ includes/Assets.php
@@ -15,5 +15,7 @@ class Assets{
     public function enqueue_assets()
     {
         wp_enqueue_script('shc-script', SHC_ASSETS.'/js/frontend.js', false, filemtime( SHC_PATH.'/assets/js/fronend.js' ), true);
+
+        wp_enqueue_style('shc-script', SHC_ASSETS.'/css/frontend.css', false, filemtime( SHC_PATH.'/assets/css/fronend.css' ) );
     }
 }

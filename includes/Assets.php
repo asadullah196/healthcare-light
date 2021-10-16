63ea2870f28a075c034d9624f70f8d00fef0399f
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-16 23:01:40 +0600

    New file added in assests file!

---
+++ includes/Assets.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Galib1996\HealthcareLite;
+
+/**
+ * Assets handler class
+ */
+class Assets
+{
+    function __construct()
+    {
+
+    }
+    public function enqueue_assets()
+    {
+        
+    }
+}

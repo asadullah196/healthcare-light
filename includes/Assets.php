238d7c2ec59439436d0ef38fd7ea98e1e95d6420
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-17 23:14:31 +0600

    Created CSS errror to check how error work, hu

--- includes/Assets.php
+++ includes/Assets.php
@@ -12,9 +12,58 @@ class Assets{
         add_action('admin_enqueue_scripts', [$this, 'enqueue_assets']);
     }
 
+    /**
+     * Return all the scripts like js
+     * 
+     * @return array
+     */
+
+    public function get_scripts(){
+        return [
+            'shc-script' => [
+                'src' => SHC_ASSETS.'/js/frontend.js',
+                'version' => filemtime( SHC_PATH.'/assets/js/fronend.js' ),
+                'deps' => ['jquery'],
+            ]
+            
+        ];
+    }
+
+    /**
+     * Return all the style like css
+     * 
+     * @return array
+     */
+
+    public function get_styles(){
+        return [
+            'shc-style' => [
+                'src' => SHC_ASSETS.'/css/frontend.css',
+                'version' => filemtime( SHC_PATH.'/assets/css/fronend.css' ),
+            ]
+        ];
+    }
+
     public function enqueue_assets()
     {
-        wp_register_script('shc-script', SHC_ASSETS.'/js/frontend.js', false, filemtime( SHC_PATH.'/assets/js/fronend.js' ), true);
-        wp_register_style('shc-style', SHC_ASSETS.'/css/frontend.css', false, filemtime( SHC_PATH.'/assets/css/fronend.css' ) );
+        /**
+         * Register all the JS file
+         */
+        $scripts = $this->get_scripts();
+        foreach ( $scripts as $handle => $scripts)
+        {
+            $deps = isset ( $scripts['deps']) ? $scripts['deps'] : false;
+            wp_register_script( $handle, $scripts['src'], $deps, $scripts, true);
+        }
+
+        /**
+         * Register all the CSS file
+         */
+        $styles = $this->get_styles();
+        foreach ( $styles as $handle => $styles)
+        {
+            $deps = isset ( $styles['deps']) ? $styles['deps'] : false;
+            wp_register_style( $handle, $styles['src'], $deps, $styles, );
+        }
     }
 }

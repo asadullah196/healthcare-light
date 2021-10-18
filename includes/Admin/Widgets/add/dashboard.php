2b90eb0eef5cb0e69eabd6e42ce73e2ece7ad050
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-18 22:17:02 +0600

    Icon fixed and now need to finish the card design.

--- includes/Admin/Widgets/add/dashboard.php
+++ includes/Admin/Widgets/add/dashboard.php
@@ -21,11 +21,10 @@ echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@
             <div class="text-white col-md-3 admin-dashboard-card">
                 <div class="row">
                     <div class="col-md-3">
-                    <i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i>
+                        <i class="bi bi-people-fill"></i>
                     </div>
                     <div class="col-md-9">
-                        <h1><span><i class="bi bi-arrow-right-square-fill"></i></span><i class="bi bi-arrow-right-square-fill"></i>Total Patients</h1>
-                        <i class="bi-github" role="img" aria-label="GitHub"></i>
+                        <h1><span>Total Patients</h1>
                         <p>05</p>
                     </div>
                 </div>

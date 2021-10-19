f47a577272a0e27e9614b24895fe65dc1905b846
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-19 22:23:45 +0600

    Admin pannel UI update going on

--- includes/Admin/Widgets/add/admin-panel.php
+++ includes/Admin/Widgets/add/admin-panel.php
@@ -1,15 +1,18 @@
 <div class="wrap">
-    <div class="admin-heading row">
-        <h1 class="wp-heading-inline"><?php _e('Admin Dashboard!', 'healthcare-lite'); ?></h1>
-        <br /><br /><br />
+    <div class="row">
+        <div class="admin-heading row">
+            <h1 class="wp-heading-inline"><?php _e('Admin Dashboard!', 'healthcare-lite'); ?></h1>
+            <br /><br /><br />
+        </div>
     </div>
 
-    <div class="">
+    <div class="row">
+        <div class="admin-area">
+            <a href="<?php echo admin_url('admin.php?page=healthcare-lite&action=add'); ?>" class="page-title-action"><?php _e('Add Patient', 'healthcare-lite'); ?></a>
 
-        <a href="<?php echo admin_url('admin.php?page=healthcare-lite&action=add'); ?>" class="page-title-action"><?php _e('Add Patient', 'healthcare-lite'); ?></a>
+            <a href="<?php echo admin_url('admin.php?page=healthcare-lite&action=view'); ?>" class="page-title-action"><?php _e('View Patients', 'healthcare-lite'); ?></a>
 
-        <a href="<?php echo admin_url('admin.php?page=healthcare-lite&action=view'); ?>" class="page-title-action"><?php _e('View Patients', 'healthcare-lite'); ?></a>
-
-        <h1 class="madamf">Sorry Doctor Bhai!</h1>
+            <h1 class="madamf">Sorry Doctor Bhai!</h1>
+        </div>
     </div>
 </div>

bf0469d58d565f36460ad252e2899185385fba89
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-19 22:21:10 +0600

    Doctore panel fixing updated!

--- includes/Admin/Widgets/add/doctor-panel.php
+++ includes/Admin/Widgets/add/doctor-panel.php
@@ -1,14 +1,10 @@
+
 <div class="wrap">
-    <div class="admin-heading row">
-        <h1 class="wp-heading-inline"><?php _e('Admin Dashboard!', 'healthcare-lite'); ?></h1>
-        <br /><br /><br />
-    </div>
+    <h1 class="wp-heading-inline"><?php _e('Hello Doctor!','healthcare-lite');?></h1>
 
-    <div class="">
-        <a href="<?php echo admin_url('admin.php?page=healthcare-lite&action=add'); ?>" class="page-title-action"><?php _e('Add Patient', 'healthcare-lite'); ?></a>
+    <a href="<?php echo admin_url( 'admin.php?page=healthcare-lite&action=add' ); ?>" class="page-title-action"><?php _e( 'Add Patient', 'healthcare-lite' ); ?></a>
 
-        <a href="<?php echo admin_url('admin.php?page=healthcare-lite&action=view'); ?>" class="page-title-action"><?php _e('View Patients', 'healthcare-lite'); ?></a>
+    <a href="<?php echo admin_url( 'admin.php?page=healthcare-lite&action=view' ); ?>" class="page-title-action"><?php _e( 'View Patients', 'healthcare-lite' ); ?></a>
 
-        <h1 class="madamf">Sorry Doctor Bhai!</h1>
-    </div>
+    <h1 class="madamf">Sorry Doctor Bhai!</h1>
 </div>

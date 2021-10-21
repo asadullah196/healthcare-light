14b4985554296a9581f1826e81b9c093ed30e6f8
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-21 18:44:47 +0600

    Appointment form added!

--- includes/Admin/Widgets/add/add-appointment.php
+++ includes/Admin/Widgets/add/add-appointment.php
@@ -1,1 +1,13 @@
-<h1></h1>
+<h1>Book your appointment</h1>
+<p>We will confirm your appointment within 2 hours.</p>
+
+<p>Name*</p>
+<p>Phone*</p>
+<p>Gender*</p>
+<p>Date of Birth*</p>
+<p>Address*</p>
+<p>Have you previously attended our facility?*</p>
+<p>Appointment Type*</p>
+<p>Date*</p>
+<p>Your Message*</p>
+<p>Book An Appointment</p>

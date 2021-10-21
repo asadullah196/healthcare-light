e12e02693f65914c7830f4e456a07e71a90a1050
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-21 16:37:54 +0600

    Schema added and schema copid from tutorial

--- includes/Installer.php
+++ includes/Installer.php
@@ -31,5 +31,17 @@ class Installer
     public function create_tables()
     {
         global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $schema = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}ac_addresses` (
+          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+          `name` varchar(100) NOT NULL DEFAULT '',
+          `address` varchar(255) DEFAULT NULL,
+          `phone` varchar(30) DEFAULT NULL,
+          `created_by` bigint(20) unsigned NOT NULL,
+          `created_at` datetime NOT NULL,
+          PRIMARY KEY (`id`)
+        ) $charset_collate";
     }
 }

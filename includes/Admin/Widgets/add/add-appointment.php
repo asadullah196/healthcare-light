d7a145e443d940b66dfd0e0c71da70fcad2f84f6
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-21 20:24:20 +0600

    Form updated and front end handling learning going on

--- includes/Admin/Widgets/add/add-appointment.php
+++ includes/Admin/Widgets/add/add-appointment.php
@@ -12,47 +12,35 @@ wp_enqueue_style('shc-admin-style');
 echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">';
 
 ?>
-
 <div class="wrap">
-
-    <div class="admin-topmenu">
-        <div class="admin-heading">
-            <div class="row">
-                <h1 class="wp-heading-inline"><?php _e('Admin Dashboard!', 'healthcare-lite'); ?></h1>
-                <br /><br />
-            </div>
+    <div class="admin-heading col">
+        <div class="row">
+            <h1 class="wp-heading-inline"><?php _e('Admin Dashboard!', 'healthcare-lite'); ?></h1>
+            <br /><br />
         </div>
     </div>
-    <div class="appointment-panel">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="row">
-                    <h1>Book your appointment</h1><br />
-                    <p>We will confirm your appointment within 2 hours.</p><br />
-                </div>
-            </div>
-            <div class="col-md-8 offset-md-1">
-                <div class="row">
-                    <div class="appointment-form">
-                        <p>Name*</p>
-                        <p>Phone*</p>
-                        <p>Gender*</p>
-                        <p>Date of Birth*</p>
-                        <p>Address*</p>
-                        <p>Have you previously attended our facility?*</p>
-                        <p>Appointment Type*</p>
-                        <p>Date*</p>
-                        <p>Your Message*</p>
-                        <p>Book An Appointment</p>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col-md-3">
-                <div class="row">
-                    <div class="appointment-schedule">
-                        Time Goes Here
-                    </div>
+    <div class="new-appointment">
+        <h1>Book your appointment</h1>
+        <p>We will confirm your appointment within 2 hours.</p>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="appointment-form">
+                    <p>Name*</p>
+                    <p>Phone*</p>
+                    <p>Gender*</p>
+                    <p>Date of Birth*</p>
+                    <p>Address*</p>
+                    <p>Have you previously attended our facility?*</p>
+                    <p>Appointment Type*</p>
+                    <p>Date*</p>
+                    <p>Your Message*</p>
+                    <p>Book An Appointment</p>
+                </div>
+            </div>
+            <div class="col-md-3 offset-md-1">
+                <div class="appointment-schedule">
+                    Time Goes Here
                 </div>
             </div>
         </div>

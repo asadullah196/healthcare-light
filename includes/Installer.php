779a1ad13c42424568341496a38c2a962b361ecf
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-21 16:25:04 +0600

    Table creating process starts

---
+++ includes/Installer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Galib1996\HealthcareLite;
+
+/**
+ * Installer class
+ */
+class Installer
+{
+    public function run()
+    {
+        $this->add_version();
+        $this->create_tables();
+    }
+
+    public function add_version()
+    {
+        $installed = get_option('shc_installed');
+        if (!$installed) {
+            update_option('shc_installed', time());
+        }
+
+        update_option('shc_version', SHC_VERSION);
+    }
+
+    public function create_tables()
+    {
+    }
+}

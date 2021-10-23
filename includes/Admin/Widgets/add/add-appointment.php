40c430a05170e9da808768a308c85016bfe63177
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-24 00:16:04 +0600

    Translation function going on!

--- includes/Admin/Widgets/add/add-appointment.php
+++ includes/Admin/Widgets/add/add-appointment.php
@@ -28,43 +28,43 @@ echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@
                 <form action="#" method="post">
 
                     <label for="patientName"><?php _e('Full Name:*', 'healthcare-litepa'); ?></label>
-                    <input type="text" id="patientName" name="patientName" placeholder="Your Name"><br><br>
+                    <input type="text" id="patientName" name="patientName" placeholder="<?php _e('Your Name', 'healthcare-lite'); ?>"><br><br>
 
-                    <label for="patientNumber">Phone Number:*</label>
-                    <input type="text" id="patientNumber" name="patientNumber" placeholder="Phone Number"><br><br>
+                    <label for="patientNumber">Phone Number:*<?php _e('Full Name:*', 'healthcare-litepa'); ?></label>
+                    <input type="text" id="patientNumber" name="patientNumber" placeholder="Phone Number<?php _e('Full Name:*', 'healthcare-litepa'); ?>"><br><br>
 
-                    <label for="patientEmail">Email Address:</label>
-                    <input type="email" id="patientEmail" name="patientEmail" placeholder="Email Address"><br><br>
+                    <label for="patientEmail">Email Address:<?php _e('Full Name:*', 'healthcare-litepa'); ?></label>
+                    <input type="email" id="patientEmail" name="patientEmail" placeholder="Email Address<?php _e('Full Name:*', 'healthcare-litepa'); ?>"><br><br>
 
 
-                    <label for="appDate">Appointment Date:</label>
-                    <input type="date" id="appDate" name="appDate" placeholder="Appointment Date"><br><br>
+                    <label for="appDate">Appointment Date:<?php _e('Full Name:*', 'healthcare-litepa'); ?></label>
+                    <input type="date" id="appDate" name="appDate" placeholder="Appointment Date<?php _e('Full Name:*', 'healthcare-litepa'); ?>"><br><br>
 
-                    <label for="appType">Appointment Type:</label>
+                    <label for="appType">Appointment Type:<?php _e('Full Name:*', 'healthcare-litepa'); ?></label>
                     <input type="checkbox" id="appTypeRegular" name="appTypeRegular" value="regular">
-                    <label id="widthzero" for="appType"> Regular &nbsp;&nbsp;</label>
+                    <label id="widthzero" for="appType"> Regular<?php _e('Full Name:*', 'healthcare-litepa'); ?> &nbsp;&nbsp;</label>
                     <input type="checkbox" id="appTypeUrgent" name="appTypeUrgent" value="Urgent">
-                    <label for="appTypeUrgent"> Urgent</label><br><br>
+                    <label for="appTypeUrgent"> Urgent<?php _e('Full Name:*', 'healthcare-litepa'); ?></label><br><br>
 
-                    <label for="patientGender">Gender:</label>
+                    <label for="patientGender">Gender:<?php _e('Full Name:*', 'healthcare-litepa'); ?></label>
                     <input type="radio" id="patientGenderMale" name="patientGenderMale" value="Male">
-                    <label id="widthzero" for="patientGenderMale"> Male &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    <label id="widthzero" for="patientGenderMale"> Male <?php _e('Full Name:*', 'healthcare-litepa'); ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
                     <input type="radio" id="patientGenderFemale" name="patientGenderFemale" value="Female">
-                    <label for="patientGenderFemale">Female</label><br><br>
+                    <label for="patientGenderFemale">Female<?php _e('Full Name:*', 'healthcare-litepa'); ?></label><br><br>
 
-                    <label for="lastVist">visited in last 3 month?</label>
+                    <label for="lastVist">visited in last 3 month?<?php _e('Full Name:*', 'healthcare-litepa'); ?></label>
                     <input type="radio" id="lastVistYes" name="lastVistYes" value="Last Visit">
-                    <label id="widthzero" for="lastVistYes"> Yes &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    <label id="widthzero" for="lastVistYes"> Yes <?php _e('Full Name:*', 'healthcare-litepa'); ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
                     <input type="radio" id="lastVistNo" name="lastVistNo" value="No">
-                    <label for="lastVistNo">No</label><br><br>
+                    <label for="lastVistNo">No<?php _e('Full Name:*', 'healthcare-litepa'); ?></label><br><br>
 
-                    <label for="patientAddress">Address:</label>
-                    <input type="text" id="patientAddress" name="patientAddress" placeholder="Your Address"><br><br>
+                    <label for="patientAddress">Address:<?php _e('Full Name:*', 'healthcare-litepa'); ?></label>
+                    <input type="text" id="patientAddress" name="patientAddress" placeholder="Your Address<?php _e('Full Name:*', 'healthcare-litepa'); ?>"><br><br>
 
-                    <label for="patientNote">Special Notes(if any):</label><br>
-                    <textarea id="patientNote" name="patientNote" rows="4" cols="50" placeholder="Add your specil note here!"></textarea><br><br>
+                    <label for="patientNote">Special Notes(if any):<?php _e('Full Name:*', 'healthcare-litepa'); ?></label><br>
+                    <textarea id="patientNote" name="patientNote" rows="4" cols="50" placeholder="Add your specil note here!<?php _e('Full Name:*', 'healthcare-litepa'); ?>"></textarea><br><br>
 
-                    <input type="submit" class="btn btn-primary" value="Book An Appointment"><br><br>
+                    <input type="submit" class="btn btn-primary" value="Book An Appointment<?php _e('Full Name:*', 'healthcare-litepa'); ?>"><br><br>
                 </form>
             </div>
             <div class="col-md-4">

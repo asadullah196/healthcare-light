a1d731737a6c8c0368349f9bbfd4d2ef29356ae7
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-23 16:42:01 +0600

    Form design going on!

--- includes/Admin/Widgets/add/add-appointment.php
+++ includes/Admin/Widgets/add/add-appointment.php
@@ -12,8 +12,8 @@ wp_enqueue_style('shc-admin-style');
 echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">';
 
 ?>
-<div class="wrap">
-    <div class="admin-heading">
+<div class="wrap container">
+    <div class="admin-heading container">
         <div class="row">
             <h1 class="wp-heading-inline"><?php _e('Admin Dashboard!', 'healthcare-lite'); ?></h1>
             <br /><br />
@@ -25,6 +25,21 @@ echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@
         <p>We will confirm your appointment within 2 hours.</p>
         <div class="row">
             <div class="col-md-8">
+                <form action="#" method="post">
+                    
+                        <label for="fname">Full Name:*</label>
+                        <input type="text" id="fname" name="fname" placeholder="Your Name">
+                    
+                        <label for="lname">Phone Number:*</label>
+                        <input type="text" id="lname" name="lname" placeholder="Phone Number"><br><br>
+
+                        <label for="lname">Email Address:</label>
+                        <input type="text" id="lname" name="lname" placeholder="Phone Number"><br><br>
+                    
+                    <label for="lname">Last name:</label><br>
+                    <input type="text" id="lname" name="lname" value="Doe"><br><br>
+                    <input type="submit" class="btn" value="Submit">
+                </form>
                 <div class="appointment-form">
                     <p>Name*</p>
                     <p>Phone*</p>
--- includes/Admin/Widgets/add/admin-panel.php
+++ includes/Admin/Widgets/add/admin-panel.php
@@ -13,14 +13,15 @@ echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@
 
 ?>
 
-<div class="wrap">
-    <div class="admin-heading">
+<div class="wrap container">
+
+    <div class="admin-heading container">
         <div class="row">
             <h1 class="wp-heading-inline"><?php _e('Admin Dashboard!', 'healthcare-lite'); ?></h1>
             <br /><br />
         </div>
     </div>
-    <div class="admin-dashboard">
+    <div class="admin-dashboard container">
         <div class="row">
 
             <div class="text-white col-md-3 text-center admin-dashboard-card">
@@ -75,5 +76,4 @@ echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@
         </div>
         <h1 class="madamf">Sorry Doctor Bhai!</h1>
     </div>
-</div>
 </div>

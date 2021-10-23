7f0bdc42f1b56c0fb8ae10a0a71c31cf3e995bf5
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-23 18:09:12 +0600

    Form design almost done!

--- includes/Admin/Widgets/add/add-appointment.php
+++ includes/Admin/Widgets/add/add-appointment.php
@@ -24,52 +24,59 @@ echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@
         <h1>Book your appointment</h1>
         <p>We will confirm your appointment within 2 hours.</p>
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-8 appointment-info">
                 <form action="#" method="post">
-                    
+
                     <label for="fname">Full Name:*</label>
                     <input type="text" id="fname" name="fname" placeholder="Your Name"><br><br>
-                
+
                     <label for="lname">Phone Number:*</label>
                     <input type="text" id="lname" name="lname" placeholder="Phone Number"><br><br>
 
                     <label for="lname">Email Address:</label>
                     <input type="text" id="lname" name="lname" placeholder="Phone Number"><br><br>
 
-                    
+
                     <label for="lname">Appointment Date:</label>
                     <input type="date" id="lname" name="lname" placeholder="Appointment Date"><br><br>
-                    
-                    <label for="lname">Appointment Type:</label><br>
+
+                    <label for="lname">Appointment Type:</label>
                     <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-                    <label for="vehicle1"> Regular</label><br>
+                    <label id="widthzero" for="vehicle1"> Regular &nbsp;&nbsp;</label>
                     <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
                     <label for="vehicle2"> Urgent</label><br><br>
 
                     <label for="lname">Gender:</label>
                     <input type="radio" id="html" name="fav_language" value="Male">
-                    <label for="html">Male</label>
+                    <label id="widthzero" for="html"> Male &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
                     <input type="radio" id="css" name="fav_language" value="Female">
                     <label for="css">Female</label><br><br>
 
-                    <label for="lname">Have you visit us in last 3 month?</label>
+                    <label for="lname">visited in last 3 month?</label>
                     <input type="radio" id="html" name="fav_language" value="Male">
-                    <label for="html">Yes</label>
+                    <label id="widthzero" for="html"> Yes &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
                     <input type="radio" id="css" name="fav_language" value="Female">
                     <label for="css">No</label><br><br>
 
                     <label for="lname">Address:</label>
                     <input type="text" id="lname" name="lname" placeholder="Phone Number"><br><br>
 
-                    <label for="lname">Special Notes(if any):</label>
-                    <input type="text" id="lname" name="lname" placeholder="Phone Number"><br><br>
+                    <label for="lname">Special Notes(if any):</label><br>
+                    <textarea id="w3review" name="w3review" rows="4" cols="50"></textarea><br><br>
 
-                    <input type="submit" class="btn" value="Book An Appointment">
+                    <input type="submit" class="btn" value="Book An Appointment"><br><br>
                 </form>
             </div>
-            <div class="col-md-3 offset-md-1">
+            <div class="col-md-4">
                 <div class="appointment-schedule">
-                    Time Goes Here
+                    <h3>Available Schedule<br/>In next 7 days</h3>
+                    <h4>Friday: <span>05</span></h4>
+                    <h4>Friday: <span>05</span></h4>
+                    <h4>Friday: <span>05</span></h4>
+                    <h4>Friday: <span>05</span></h4>
+                    <h4>Friday: <span>05</span></h4>
+                    <h4>Friday: <span>05</span></h4>
+                    <h4>Friday: <span>05</span></h4>
                 </div>
             </div>
         </div>

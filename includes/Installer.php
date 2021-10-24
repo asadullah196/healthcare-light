d09b7c2306325de7eb5ca8b9aa815627eff7b7b0
Asadullah Galib <md.asadullah196@gmail.com>
2021-10-24 22:47:35 +0600

    Database code updated on create table. New table added for appointment!

--- includes/Installer.php
+++ includes/Installer.php
@@ -35,12 +35,18 @@ class Installer
         $charset_collate = $wpdb->get_charset_collate();
 
         $schema = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}shc_appointment` (
-          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-          `name` varchar(100) NOT NULL DEFAULT '',
-          `address` varchar(255) DEFAULT NULL,
-          `phone` varchar(30) DEFAULT NULL,
-          `created_by` bigint(20) unsigned NOT NULL,
-          `created_at` datetime NOT NULL,
+            `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+            `name` varchar(100) NOT NULL DEFAULT '',
+            `phone` varchar(30) DEFAULT NULL,
+            `email` varchar(255) NOT NULL,
+            `appointment` date NOT NULL,
+            `type` int(10) NOT NULL,
+            `gender` int(10) NOT NULL,
+            `visited` int(10) NOT NULL,
+            `address` varchar(255) DEFAULT NULL,
+            `notes` varchar(800) NOT NULL,
+            `created_by` bigint(20) unsigned NOT NULL,
+            `created_at` datetime NOT NULL,
           PRIMARY KEY (`id`)
         ) $charset_collate";
 
@@ -48,7 +54,7 @@ class Installer
             require_once ABSPATH . 'wp-admin/includes/upgrade.php';
         }
 
-        shc_appointment();
+        //shc_appointment();
         
         dbDelta($schema);
     }
